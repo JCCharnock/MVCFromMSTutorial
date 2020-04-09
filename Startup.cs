a628b3e854feb422151dd73edaa7e88c09844460
Charnock <jcharnock@partners.org>
2020-04-09 09:33:19 -0400

    testing GitHub link

--- Startup.cs
+++ Startup.cs
@@ -21,6 +21,7 @@ namespace MvcMovie
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
+        // try to get this to GitHub
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
